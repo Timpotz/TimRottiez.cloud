e7cadf08290444fae8dae44193d30ac21f1533ca
Timpotz <timotiusp187@gmail.com>
2025-05-04 21:00:45 +0700

    ease enter the commit message for your changes. Lines starting

--- kennel_site/settings.py
+++ kennel_site/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['timrottiez.com', 'localhost', '127.0.0.1','www.timrottiez.com']
+ALLOWED_HOSTS = ['timrottiez.cloud', 'localhost', '127.0.0.1','www.timrottiez.cloud']
 
 # Application definition
 
